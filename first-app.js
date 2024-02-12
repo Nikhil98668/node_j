347775630fe2ce432a629a6abf857053662d7130
Nikhil98668 <nsd98668@gmail.com>
2024-02-12 23:16:18 +0530

    multiple-route

--- first-app.js
+++ first-app.js
@@ -3,10 +3,22 @@ const fs=require('fs');
 fs.writeFileSync('hello.txt','hello world');*/
 
 const http = require('http');
-const myName = "Nikhil";
+/*const myName = "Nikhil";
 const server = http.createServer((req, res) => {
     res.end(myName);
+});*/
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    if (url === '/home') {
+        res.end('Welcome to About Us page');
+    }
+    else if (url === '/node') {
+        res.end('Welcome to my Node.js project');
+    }
+    else {
+        res.end('404 Not Found');
+    }
 });
-server.listen(4000, () => {
-   console.log(`${myName}`);
+server.listen(3000, () => {
+    //console.log('Server is running on port 4000');
 });
