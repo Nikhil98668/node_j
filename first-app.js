9dffefafa725d67acdbb086d5421b92f613df10b
Nikhil98668 <nsd98668@gmail.com>
2024-02-12 21:27:13 +0530

    createserver

--- first-app.js
+++ first-app.js
@@ -1,3 +1,12 @@
-console.log("hello world");
+/*console.log("hello world");
 const fs=require('fs');
-fs.writeFileSync('hello.txt','hello world');
+fs.writeFileSync('hello.txt','hello world');*/
+
+const http = require('http');
+const myName = "Nikhil";
+const server = http.createServer((req, res) => {
+    res.end(myName);
+});
+server.listen(4000, () => {
+   console.log(`${myName}`);
+});
