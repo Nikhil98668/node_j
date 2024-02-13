9ef91e1399fd13a771e15b0db85cc969791a140b
Nikhil98668 <nsd98668@gmail.com>
2024-02-13 11:00:12 +0530

    export_element

---
+++ app1.js
@@ -0,0 +1,38 @@
+
+const fs=require('fs');
+const reqhandl= (req,res) => {
+    const url = req.url;
+    const method=req.method;
+    if(url==='/')
+    {
+        res.write('<html>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">submit</button></form><body>');
+        res.write('</html>');
+        return res.end();
+    }
+    //res.end('HI')
+    if(url==='/message' && method=='POST')
+    {
+        const body=[];
+        req.on('data',(chunk)=>{
+            body.push(chunk);
+            console.log(chunk);
+        });
+        return req.on('end',()=>{
+            const pars=Buffer.concat(body).toString();
+            console.log(pars);
+            const p=pars.split("=")[1];
+            //fs.writeFileSync('message.txt',p);
+            fs.writeFile('message.txt',p, err => {
+                res.statusCode=302;
+        res.setHeader('Location','/');
+        return res.end();
+
+           });
+
+        });
+
+        
+    }
+}
+module.exports={r:reqhandl};
