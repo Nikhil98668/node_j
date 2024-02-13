7022f0b07efd87b430265059dbf438897fe0f1dc
Nikhil98668 <nsd98668@gmail.com>
2024-02-13 17:54:38 +0530

    Routes

---
+++ nikk.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin'); 
+const shopRoutes = require('./routes/shop'); 
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use('/admin', adminRoutes);
+
+app.use('/shop', shopRoutes);
+
+app.use('/',(req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
