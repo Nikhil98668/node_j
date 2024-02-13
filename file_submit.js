94ebcbfdc5df5471179b632526d66196395a59bd
Nikhil98668 <nsd98668@gmail.com>
2024-02-13 15:04:04 +0530

    First_Express

--- file_submit.js
+++ file_submit.js
@@ -1,7 +1,25 @@
 const http = require('http');
-//const http = require('http');
-const app1=require('./app1.js');
-const server = http.createServer(app1.r);
+
+const express = require('express');
+
+const app = express();
+
+app.use((req,res,next)=>{
+console.log("Middleware");
+next();
+});
+app.use((req,res,next)=>{
+    console.log("Another Middleware");
+    //res.send("HIII");
+    res.send('<h1> hello to node js </h1>') 
+});
+
+
+//const app1=require('./app1.js');
+
+const server = http.createServer(app);
+
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
+
 });
