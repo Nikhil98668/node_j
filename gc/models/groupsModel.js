ff1e1c0e36934747047560275f787b3e47cf1088
Nikhil98668 <nsd98668@gmail.com>
2024-03-14 20:59:27 +0530

    live

---
+++ gc/models/groupsModel.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize')
+
+const sequelize = require('../util/database')
+
+const Groups = sequelize.define('groups', {
+    id: {
+        type: Sequelize.INTEGER,
+        allowedNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowedNull: false
+    },
+    members: {
+        type: Sequelize.TEXT,
+        allowedNull: false
+    },
+    admins: {
+        type: Sequelize.TEXT,
+        allowedNull: false
+    },
+    usersId: {
+        type: Sequelize.INTEGER,
+        allowedNull: false
+    }
+})
+
+module.exports = Groups;
